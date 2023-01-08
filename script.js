d8fb922a15e9381e17db164e647314aad22a51e8
ajringst <aj.ringstad@gmail.com>
2023-01-07 22:02:50 -0700

    make separate operator functions

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 let screenValues = new Array(3);
 
-
 const indexCheck = function(){
   if (screenValues[1] == undefined ){
   console.log(screenValues[1]);
   }
 };
+
+const add = function(a,b){
+  return a + b;
+}
+
+const subtract = function(a,b){
+  return a - b;
+}
+
+const multiply = function(a,b){
+  return a * b;
+}
+
+const divide = function(a,b){
+  if( b == 0){
+    return null;
+  }
+  else{
+    return a / b;
+  }
+}
 
 const nums = document.querySelectorAll(".number");
 const concatNum = nums.forEach(element => { element.addEventListener("click", function(){
@@ -18,21 +38,16 @@ indexCheck(screenValues);
 const operate = function([first, operator, second]){
   let a = first * 1;
   let b = second * 1;
-  if(operator == add ){
-    return a + b;
+  if(operator == "add"){
+    return add(a,b);
   };
-  if(operator == subtract ){
-    return a - b;
+  if(operator == "subtract"){
+    return subtract(a,b);
   };
-  if(operator == multiply ){
-    return a * b;
+  if(operator == "multiply"){
+    return multiply(a,b);
   };
-  if(operator == divide ){
-    if(b == 0){
-      alert("you sneaky bugger!")
-    }
-    else{
-    return a / b;
-    }
+  if(operator == "divide"){
+    return divide(a,b);
   };
 };
